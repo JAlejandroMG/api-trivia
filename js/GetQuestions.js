dfec29df271f26d9a0138a6067d4d9d035172b19
JAlejandroMG <jalejandromg@tutanota.com>
2020-10-17 18:48:18 -0500

    Fix bug submit user answers, Score, alert result

--- js/GetQuestions.js
+++ js/GetQuestions.js
@@ -63,64 +63,42 @@ export default class GetQuestions {
 
   sendDataResults(dataQuestions) {
     this.printCards(dataQuestions);
-    // score.getCorrectAnswers(dataQuestions);
+    getCorrectAnswers(dataQuestions);
   }
 
 
   printCards(questions) {
-    const container = document.getElementById('container-cards');
+    const container = document.getElementById('questions-cards');
     container.innerHTML = '';
+
     questions.forEach((question, qIndex) => {
         const card = this.returnCardHTML(question, qIndex);
         container.innerHTML += card;
     });
-    container.innerHTML += `<button type="button" class="btn btn-primary btn-lg btn-block mt-5 mb-5 col-lg-8 col-md-8 col-sm-12">Block level button</button>`;
+  
+    container.innerHTML += `<button type="submit" class="btn btn-primary btn-lg btn-block mt-5 mb-5 col-md-6">Block level button</button>`;
     //* Poner las preguntas y el botón de submit de las respuestas en la página web
   }
 
 
   returnCardHTML(q, qIndex) {
-    const card = `<div class="card col-lg-5 col-md-5 col-sm-12 mt-3">
+    const card = `<div class="card col-md-6 col-lg-4 mt-3">
                     <div class="card-body">
                     <h5 class="card-title">${q.category}</h5>
                     <h6 class="card-subtitle mb-2 text-muted">${q.question}</h6>
-                        ${this.returnAnswersHTML(q.type, q.correct_answer, q.incorrect_answers, qIndex)}           
-                    </div>
-                </div>`
+                    ${this.returnAnswersHTML(q.correct_answer, q.incorrect_answers, qIndex, q.type)}
+                  </div>`
     return card;
   }
 
 
-  returnAnswersHTML(type, correct, incorrects, qIndex) {
+  returnAnswersHTML(correct, incorrects, qIndex, type) {
     incorrects.push(correct);
-    let orderedArray = [];
 
-    if(type === "multiple") {
-      function newOrder() {
-        const min = 0;
-        let max = 4;      
-        let index = 0;
-        const newOrder = [];
-      
-        for(let i = 0; i < 4; i++) {
-          function randomNumber(min, max) {
-            return Math.floor(Math.random() * (max - min) + min);
-          }
-          index = (randomNumber(min, max));
-          newOrder.push(incorrects[index]);
-          incorrects.splice(index, 1);
-          max--;
-        }
-      
-        return newOrder;
-      }
-      orderedArray = newOrder()
-    } else {
-        orderedArray = incorrects;
-      }
+    incorrects.sort(function(a, b){return 0.5 - Math.random()}); 
 
     let answersHTML = '';
-    orderedArray.forEach((ordered, aIndex) => {
+    incorrects.forEach((ordered, aIndex) => {
         answersHTML += `<div class="form-check">
                             <input class="form-check-input" type="radio" name="question-${qIndex}" id="answer-${qIndex}${aIndex}" value="${ordered}" required>
                             <label class="form-check-label" for="answer-${qIndex}${aIndex}">
--- js/main.js
+++ js/main.js
@@ -3,9 +3,11 @@ import GetQuestions from './GetQuestions.js';
 import GetScore from './GetScore.js';
 
 
+
 const categories = new GetCategories();
 const questions = new GetQuestions();
 const score = new GetScore();
+
 
 
 //@ -------------- Aquí escuchamos el evento submit de la forma -------------- @//
@@ -15,12 +17,46 @@ document.getElementById('questions-form').addEventListener('submit', (event) => 
 })
 
 
+
 //@ ----------- Aquí escuchamos el evento submit de las respuestas ----------- @//
 document.getElementById('questions-cards').addEventListener('submit', (event) => {
   event.preventDefault();
-  alert('Al entregar respuestas...');
-  score.getUserAnswers();
+  getUsersAnswers();
 })
 
 
+
+//@ Aquí creamos el arreglo de respuestas correctas
+const correctAnswers = [];
+
+function getCorrectAnswers(dataResults) {
+  dataResults.forEach(dataResult => {
+    correctAnswers.push(dataResult.correct_answer);
+  });
+}
+
+
+
+//@ Aquí creamos el arreglo de las respuestas del usuario
+const usersAnswers = [];
+
+function getUsersAnswers(){
+  for(let i = 0; i < correctAnswers.length; i++) {  
+    const answers = document.getElementsByName(`question-${i}`); 
+    
+    answers.forEach(answer => {
+      if(answer.checked) {
+        usersAnswers.push(answer.value);
+      }
+    })
+  }
+
+  const result = score.getScore(correctAnswers, usersAnswers)
+  score.giveResults(result);
+}
+
+
+
 categories.getCategories();
+
+window.getCorrectAnswers = getCorrectAnswers;
